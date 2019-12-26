41a3c8051229aa5599e864a95be915e1449b8d18
ksy <ksy8230@hanmail.net>
2019-12-26 22:18:53 +0900

    가위바위보 만들기

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 
-import ResponseCheck from './ResponseCheck';
+import RSP from './RSP';
 
-const Hot = hot(ResponseCheck);
+const Hot = hot(RSP);
 
 ReactDOM.render(<Hot />, document.getElementById('root'));
