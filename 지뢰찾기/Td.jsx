70b1b8c6f277d40c1282bb010dcfe14d040f2d30
ksy <ksy8230@hanmail.net>
2019-12-28 21:27:48 +0900

    swich문으로 값에 따라 style 입히기

--- 지뢰찾기/Td.jsx
+++ 지뢰찾기/Td.jsx
@@ -1,18 +1,48 @@
 import React, { useContext } from 'react';
-import { TableContext } from './mineSearch';
+import { TableContext, CODE } from './mineSearch';
 
 const getTdStyle = (code) => {
-
+    switch (code) {
+        case CODE.NORMAL :
+            return {
+                background:'#eee'
+            }
+        case CODE.MINE :
+            return {
+                background: '#666'
+            }
+        case CODE.OPENED :
+            return {
+                background : '#fff'
+            }
+        default : 
+        return {
+            background : '#eee'
+        }
+    }
 };
 
 const getTdText = (code) => {
-
+    switch (code) {
+        case CODE.NORMAL :
+            return '';
+        case CODE.MINE : 
+            return 'x';
+        case CODE.OPENED:
+            return '';
+        default:
+            return '';
+    }
 };
 
 const Td = ({rowIndex, cellIndex}) => {
     const { tableData } = useContext(TableContext);
   return (
-    <td>{tableData[rowIndex][cellIndex]}</td>
+    <td
+        style={getTdStyle(tableData[rowIndex][cellIndex])}
+    >
+        {getTdText(tableData[rowIndex][cellIndex])}
+    </td>
   );
 }
 
